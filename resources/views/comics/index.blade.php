c56e527040e350c5fcc6ebbd0a7c4b51dc0b5430
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-25 19:42:32 +0200

    adding comics to index.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,6 +3,24 @@
 @section('title', 'Comics')
 
 @section('main')
+<section id='comics'>
 
+  <div class="container">
+      <div class="current-series button">CURRENT SERIES</div>
+
+      <div class="flex-container">
+
+          @foreach ($comics as $comic)
+              <div class="card">
+                  <a href="{{ route('comics.show', $comic) }}">
+                      <figure>
+                          <img class="comic-thumb" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                      </figure>
+                      <h4>{{ $comic['title'] }}</h4>
+                  </a>
+              </div>
+          @endforeach
+
+      </div>
 
 @endsection
