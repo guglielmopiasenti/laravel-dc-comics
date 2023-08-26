a7a66a1fd9fa17fe858daf13158e6f3543968224
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-26 21:17:30 +0200

    adding btn for detail

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -14,6 +14,9 @@
                         </figure>
                         <h4>{{ $comic['title'] }}</h4>
                     </a>
+                    <div>
+                      <a class="btn btn-primary btn-sm" href="{{route{['comics.show', $comic]}}}">See Details</a>
+                    </div>
                 </div>
             @endforeach
         </div>
