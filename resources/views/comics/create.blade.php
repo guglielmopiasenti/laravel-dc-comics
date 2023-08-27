0da6087880ce7dcb2ad92d28746742446977bf27
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-27 18:35:59 +0200

    first input form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -8,6 +8,16 @@
   <a href="{{route('teams.index')}}" class="btn-secondary">Go back</a>
 </div>
 <div class="card-body">
-  
+  {{-- Form --}}
+  <form>
+    <div class="row">
+      <div class="col-6">
+        <div class="mb-3">
+          <label for="title" class="form-label">Title</label>
+          <input type="text" class="form-control" id="title" placeholder="Title">
+        </div>
+      </div>
+    </div>
+  </form>
 </div>
 @endsection
