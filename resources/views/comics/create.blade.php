52ba303c9815d377bd7a50c69b9056e396b3e4aa
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-27 07:57:27 +0200

    first structure

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.main')
+
+@section('title', 'Add comic')
+
+@section('content')
+<div class="card-title d-flex align-items-center justify-content-center">
+  <h1>Add comic</h1>
+  <a href="{{route('teams.index')}}" class="btn-secondary">Go back</a>
+</div>
+<div class="card-body">
+  
+</div>
+@endsection
