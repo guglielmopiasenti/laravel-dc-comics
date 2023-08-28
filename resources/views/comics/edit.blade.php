e9644b2353d39fd2e938ed6a2027309a1c0e2128
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-28 18:09:15 +0200

    edit first draft

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.main')
+
+@section('title', 'Edit comic')
+
+@section('main-content')
+<div class="container">
+    <div class="card-title d-flex align-items-center justify-content-center">
+        <h1 class="my-3 mx-5">Edit comic</h1>
+        <a href="{{ route('comics') }}" class="btn btn-secondary">Go back</a>
+    </div>
+    <div class="card-body">
+        <form action="{{ route('comics.update', $comic->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            <div class="row">
+                <div class="col-6">
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}" required>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea class="form-control" id="description" name="description" required>{{ $comic->description }}</textarea>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+                        <label for="thumb" class="form-label">Thumb</label>
+                        <input type="url" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}" required>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Price</label>
+                        <input type="number" class="form-control" id="price" name="price" value="{{ $comic->price }}" min="0" required>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+                        <label for="series" class="form-label">Series</label>
+                        <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}" required>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">Sale Date</label>
+                        <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}" required>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+                        <label for="type" class="form-label">Type</label>
+                        <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}" required>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+                        <label for="artists" class="form-label">Artists</label>
+                        <input type="text" class="form-control" id="artists" name="artists" value="{{ $comic->artists }}" required>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="mb-3">
+                        <label for="writers" class="form-label">Writers</label>
+                        <input type="text" class="form-control" id="writers" name="writers" value="{{ $comic->writers }}" required>
+                    </div>
+                </div>
+                <div class="col-12 text-end">
+                    <div class="mb-3">
+                        <button type="reset" class="btn btn-warning">Reset</button>
+                        <button type="submit" class="btn btn-success">Update</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
